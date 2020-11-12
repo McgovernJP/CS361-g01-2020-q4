591d559b5f32c609612bf4f3acdc49a8bf871a8d
James McGovern <jmcgovern532@gmail.com>
2020-11-12 10:20:06 -0500

    added node structure

---
+++ project.js
@@ -0,0 +1,56 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({secret:'SessionSecret'}));
+app.use(express.static('public'));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 33037);
+
+
+
+
+app.get('/',function(req,res){
+  res.render('home.handlebars', {title: 'Home page'})
+});
+
+app.get('/trailsnearme',function(req,res){
+  res.render('trailsnearme.handlebars', {title: 'Trails Near Me'})
+});
+
+app.get('/personalized',function(req,res){
+  res.render('personalized.handlebars', {title: 'Trails For Me'})
+});
+
+app.get('/fitness',function(req,res){
+  res.render('fitness.handlebars', {title: 'Estimate Fitness Level'})
+});
+
+app.get('/recommendation',function(req,res){
+  res.render('recommendation.handlebars', {title: 'Gear Recommendation'})
+});
+
+
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  //console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
